e7d1cb231556eb97df1296a9db25f6246476cc82
NikolG <greplova.nikol@gmail.com>
2026-03-08 00:00:41 +0100

    Changing styles

---
+++ HTML/backgrounds.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    let body = document.querySelector('body');
+    document.querySelector('#red').addEventListener('click', function() {
+        body.style.backgroundColor = 'red';
+    });
+    document.querySelector('#green').addEventListener('click', function() {
+        body.style.backgroundColor = 'green';
+    });
+    document.querySelector('#blue').addEventListener('click', function() {
+        body.style.backgroundColor = 'blue';
+    });        
+});
